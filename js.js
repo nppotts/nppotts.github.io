2385f3f7f211681d9b459dba7800329a4df40b01
nppotts <nichole.potts.design@gmail.com>
2019-06-27 00:23:30 -0400

    site

---
+++ js.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("nav").html('<ul><li class="l home"><a href="index.html">Home</a></li><li class="navLinks r"><a href="projects.html">Projects</a></li><li class="navLinks r"><a href="contact.html">Contact</a></li></ul>');
+
+    $(".contactInfo").html('<a href = mailto:"nichole.potts.design@gmail.com">nichole.potts.design@gmail.com</a>\
+    /\
+    <a href="https://www.dropbox.com/s/6x35k1r07qlmj9c/NicholePotts_GraphicDesigner.pdf?dl=0">resume</a>');
+
+});
